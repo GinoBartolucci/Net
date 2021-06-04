db1c5370d13c10338e9748df89a80b46b476f461
Gino Bartolucci <ginobartolucci00@gmail.com>
2021-06-04 19:31:35 -0300

    U2 Lab2.2

---
+++ Unidad02/Capitulo02/LabClases1/Clases/A.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class A
+    {
+        private string nombreInstancia;
+
+        public A()
+        {
+            nombreInstancia = "Instancia sin nombre";
+        }
+        public A(string nombre)
+        {
+            nombreInstancia = nombre;
+        }
+        public void MostrarNombre()
+        {
+            Console.WriteLine(nombreInstancia);
+        }
+        protected void M1()
+        {
+            Console.WriteLine("El metodo 1 fue invocado");
+        }
+        public void M2()
+        {
+            Console.WriteLine("El metodo 2 fue invocado");
+        }
+        public void M3()
+        {
+            Console.WriteLine("El metodo 3 fue invocado");
+        }
+
+    }
+}
---
+++ Unidad02/Capitulo02/LabClases1/LabClases1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace LabClases1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A instanciaA = new A();
+
+            B instanciaB = new B();
+
+            instanciaB.M1();
+            instanciaB.MostrarNombre();
+
+            Console.ReadLine();
+        }
+    }
+}
