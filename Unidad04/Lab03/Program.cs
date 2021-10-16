c9addb36771dfc3084303ba9135b15c48bff61eb
Gino Bartolucci <ginobartolucci00@gmail.com>
2021-10-16 05:46:10 -0300

    Unidad 04 - Lab03

--- Unidad04/Lab03/Program.cs
+++ Unidad04/Lab03/Program.cs
@@ -35,10 +35,74 @@ namespace Lab03
             rwAlumnos[colNombre] = "Leo";
             dtAlumnos.Rows.Add(rwAlumnos);
 
-            DataRow rwAlumnos2 = dtAlumnos.NewRow();
-            rwAlumnos2[colApellido] = "Maradona";
-            rwAlumnos2[colNombre] = "Diego";
-            dtAlumnos.Rows.Add(rwAlumnos2);
+            rwAlumnos = dtAlumnos.NewRow();
+            rwAlumnos[colApellido] = "Maradona";
+            rwAlumnos[colNombre] = "Diego";
+            dtAlumnos.Rows.Add(rwAlumnos);
+
+            //Cursos
+
+            DataTable dtCursos = new DataTable("Cursos");
+            DataColumn colIDCurso = new DataColumn("IDCurso", typeof(int));
+            colIDCurso.ReadOnly = true;
+            colIDCurso.AutoIncrement = true;
+            colIDCurso.AutoIncrementSeed = 1;
+            colIDCurso.AutoIncrementStep = 1;
+            DataColumn colCurso = new DataColumn("Curso", typeof(String));
+            dtCursos.Columns.Add(colIDCurso);
+            dtCursos.Columns.Add(colCurso);
+            dtCursos.PrimaryKey = new DataColumn[] { colIDCurso };
+
+            DataRow rwCurso = dtCursos.NewRow();
+            rwCurso[colCurso] = "Informatica";
+            dtCursos.Rows.Add(rwCurso);
+
+            //Universidad
+
+            DataSet dsUniversidad = new DataSet();
+            dsUniversidad.Tables.Add(dtAlumnos);
+            dsUniversidad.Tables.Add(dtCursos);
+
+            //Relacion
+
+            DataTable dtAlumnos_Cursos = new DataTable("Alumnos_Cursos");
+            DataColumn col_ac_IDAlumno = new DataColumn("IDAlumno", typeof(int));
+            DataColumn col_ac_IDCurso = new DataColumn("IDcurso", typeof(int));
+            dtAlumnos_Cursos.Columns.Add(col_ac_IDCurso);
+            dtAlumnos_Cursos.Columns.Add(col_ac_IDAlumno);
+
+            dsUniversidad.Tables.Add(dtAlumnos_Cursos);
+
+            //Relacion del dataSet
+            DataRelation relAlumno_ac = new DataRelation("Alumno_Curso", colIDAlumno, col_ac_IDAlumno);
+            DataRelation relCurso_ac = new DataRelation("Curso_Alumno", colIDCurso, col_ac_IDCurso);
+          
+            dsUniversidad.Relations.Add(relAlumno_ac);
+            dsUniversidad.Relations.Add(relCurso_ac);
+
+            DataRow rwAlumnosCursos = dtAlumnos_Cursos.NewRow();
+            rwAlumnosCursos[col_ac_IDAlumno] = 0;
+            rwAlumnosCursos[col_ac_IDCurso] = 1;
+            dtAlumnos_Cursos.Rows.Add(rwAlumnosCursos);
+
+            rwAlumnosCursos = dtAlumnos_Cursos.NewRow();
+            rwAlumnosCursos[col_ac_IDAlumno] = 1;
+            rwAlumnosCursos[col_ac_IDCurso] = 1;
+            dtAlumnos_Cursos.Rows.Add(rwAlumnosCursos);
+
+            Console.Write("Ingrese nombre del curso: ");
+            string materia = Console.ReadLine();
+            Console.WriteLine("Listado de alimnos del curso " + materia);
+            DataRow[] row_cursoInf = dtCursos.Select("Curso = '" + materia + "' ");
+            foreach(DataRow rowCu in row_cursoInf)
+            {
+                DataRow[] row_AlumnosInf = rowCu.GetChildRows(relCurso_ac);
+                foreach(DataRow rowAl in row_AlumnosInf)
+                {
+                    Console.WriteLine( rowAl.GetParentRow(relAlumno_ac)[colApellido].ToString() 
+                        +", "+ rowAl.GetParentRow(relAlumno_ac)[colNombre].ToString());
+                }
+            }
 
             Console.ReadKey();
         }
---
+++ Unidad04/Lab03_Ejercicio04/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Lab03_Ejercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtEmpresas = new DataTable("Empresas");
+            dtEmpresas.Columns.Add("CustomerID", typeof(string));
+            dtEmpresas.Columns.Add("CompanyName", typeof(string));
+
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=LOCALHOST;Initial Catalog=Northwind; User ID=sa;Pwd=123";
+
+            SqlDataAdapter myadap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
+            myconn.Open();
+            myadap.Fill(dtEmpresas);
+            myconn.Close();
+
+            Console.WriteLine("Listado de Empresas: ");
+            foreach (DataRow rowEmpresa in dtEmpresas.Rows)
+            {
+                string idempresa = rowEmpresa["CustomerID"].ToString();
+                string nombreempresa = rowEmpresa["CompanyName"].ToString();
+                Console.WriteLine(idempresa + " - " + nombreempresa);
+            }
+
+            Console.Write("Escriba el CustomerID que desea modificar: ");
+            string custid = Console.ReadLine();
+
+            DataRow[] rwempresas = dtEmpresas.Select("CustomerID= '" + custid + "'");
+            if(rwempresas.Length != 1)
+            {
+                Console.WriteLine("CustomerID no encontrado");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                DataRow rowMiEmpresa = rwempresas[0];
+                string nombreactual = rowMiEmpresa["CompanyName"].ToString();
+                Console.WriteLine("Nombre actual de la empresa: " + nombreactual);
+                Console.Write("Escriba el nuevo nombre: ");
+                string nuevonombre = Console.ReadLine();
+
+                rowMiEmpresa.BeginEdit();
+                rowMiEmpresa["CompanyName"] = nuevonombre;
+                rowMiEmpresa.EndEdit();
+
+                SqlCommand upcommand = new SqlCommand();
+                upcommand.Connection = myconn;
+                upcommand.CommandText = "UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID = @CustomerID";
+                upcommand.Parameters.Add("@CompanyName", SqlDbType.NVarChar, 50, "CompanyName");
+                upcommand.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 5, "CustomerID");
+
+                myadap.UpdateCommand = upcommand;
+                myadap.Update(dtEmpresas);
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+        private void Ejercicio_04_03()
+        {
+            DataTable dtEmpresas = new DataTable("Empresas");
+            dtEmpresas.Columns.Add("CustomerID", typeof(string));
+            dtEmpresas.Columns.Add("CompanyName", typeof(string));
+
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=LOCALHOST;Initial Catalog=Northwind; User ID=sa;Pwd=123";
+
+            SqlDataAdapter myadap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
+            myconn.Open();
+            myadap.Fill(dtEmpresas);
+            myconn.Close();
+
+            Console.WriteLine("Listado de Empresas: ");
+            foreach (DataRow rowEmpresa in dtEmpresas.Rows)
+            {
+                string idempresa = rowEmpresa["CustomerID"].ToString();
+                string nombreempresa = rowEmpresa["CompanyName"].ToString();
+                Console.WriteLine(idempresa + " - " + nombreempresa);
+            }
+
+            Console.Write("Escriba el CustomerID que desea modificar: ");
+            string custid = Console.ReadLine();
+
+            DataRow[] rwempresas = dtEmpresas.Select("CustomerID= '" + custid + "'");
+            if (rwempresas.Length != 1)
+            {
+                Console.WriteLine("CustomerID no encontrado");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                DataRow rowMiEmpresa = rwempresas[0];
+                string nombreactual = rowMiEmpresa["CompanyName"].ToString();
+                Console.WriteLine("Nombre actual de la empresa: " + nombreactual);
+                Console.Write("Escriba el nuevo nombre: ");
+                string nuevonombre = Console.ReadLine();
+
+                rowMiEmpresa.BeginEdit();
+                rowMiEmpresa["CompanyName"] = nuevonombre;
+                rowMiEmpresa.EndEdit();
+
+                SqlCommand upcommand = new SqlCommand();
+                upcommand.Connection = myconn;
+                upcommand.CommandText = "UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID = @CustomerID";
+                upcommand.Parameters.Add("@CompanyName", SqlDbType.NVarChar, 50, "CompanyName");
+                upcommand.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 5, "CustomerID");
+
+                myadap.UpdateCommand = upcommand;
+                myadap.Update(dtEmpresas);
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+        private void Ejercicio_04_02()
+        {
+            DataTable dtEmpresas = new DataTable("Empresas");
+            dtEmpresas.Columns.Add("CustomerID", typeof(string));
+            dtEmpresas.Columns.Add("CompanyName", typeof(string));
+
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=LOCALHOST;Initial Catalog=Northwind; User ID=sa;Pwd=123";
+
+            SqlDataAdapter myadap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
+            myconn.Open();
+            myadap.Fill(dtEmpresas);
+            myconn.Close();
+
+            Console.WriteLine("Listado de Empresas: ");
+            foreach (DataRow rowEmpresa in dtEmpresas.Rows)
+            {
+                string idempresa = rowEmpresa["CustomerID"].ToString();
+                string nombreempresa = rowEmpresa["CompanyName"].ToString();
+                Console.WriteLine(idempresa + " - " + nombreempresa);
+            }
+            Console.ReadLine();
+
+        }
+        private void Ejercicio_04_01()
+        {
+            DataTable dtEmpresas = new DataTable("Empresas");
+            dtEmpresas.Columns.Add("CustomerID", typeof(string));
+            dtEmpresas.Columns.Add("CompanyName", typeof(string));
+
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=LOCALHOST;Initial Catalog=Northwind; User ID=sa;Pwd=123";
+
+            SqlCommand mycommand = new SqlCommand();
+            mycommand.CommandText = "SELECT CustomerID, CompanyName FROM Customers";
+            mycommand.Connection = myconn;
+
+            SqlDataAdapter myadap = new SqlDataAdapter(mycommand.CommandText, mycommand.Connection);
+
+            myconn.Open();
+            SqlDataReader mydr = mycommand.ExecuteReader();
+            dtEmpresas.Load(mydr);
+            myconn.Close();
+
+            Console.WriteLine("Listado de Empresas: ");
+            foreach (DataRow rowEmpresa in dtEmpresas.Rows)
+            {
+                string idempresa = rowEmpresa["CustomerID"].ToString();
+                string nombreempresa = rowEmpresa["CompanyName"].ToString();
+                Console.WriteLine(idempresa + " - " + nombreempresa);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
